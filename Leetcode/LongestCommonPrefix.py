16f02f782cda865b23e2e49ca1021e20e0844df2
Leiyang <Leiyang@mokak.com>
2017-03-27 00:06:11 +0800

    Updated

---
+++ Leetcode/LongestCommonPrefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix=''
+        i=0
+        while(len(strs)>0):
+            for j in range(len(strs)):
+                if(i>=len(strs[j])) or (j>0 and strs[j][i]!=strs[j-1][i]):
+                    return prefix
+            prefix+=strs[0][i]
+            i+=1
+        return prefix
+        
+        
---
+++ Leetcode/RomanToInterger.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s=='':
+            return 0
+        sum=0
+        sum+=s.count('M')*1000
+        sum+=s.count('D')*500
+        sum+=s.count('C')*100
+        sum+=s.count('L')*50
+        sum+=s.count('X')*10
+        sum+=s.count('V')*5
+        sum+=s.count('I')
+        if 'IV' in s:
+            sum-=2
+        if 'IX' in s:
+            sum-=2
+        if 'XL' in s:
+            sum-=20
+        if 'XC' in s:
+            sum-=20
+        if 'CD' in s:
+            sum-=200
+        if 'CM' in s:
+            sum-=200
+        return sum
