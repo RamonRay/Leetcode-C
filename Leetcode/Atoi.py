12b21303c09bf92275fb047a978e9870bccac625
Leiyang <Leiyang@mokak.com>
2017-03-26 20:55:56 +0800

    2 new tests

---
+++ Leetcode/Atoi.py
@@ -0,0 +1,24 @@
+def myAtoi(str):
+    """
+    :type str: str
+    :rtype: int
+    """
+    sign=1
+    base=0
+    i=0
+    if str=='':
+        return 0
+    while(str[i]==' '):i+=1
+    if(str[i]=='-'):
+        sign=-1
+    if(str[i]=='+'):
+        sign=1
+    while(i<len(str) and str[i]>='0' and str[i]<='9' ):
+        if(base>214748364 or (base==214748364 and int(str[i])>7)):
+            if(sign==1):
+                return 2147483647
+            if(sign==-1):
+                return -2147483647
+        base=10*base+int(str[i])
+        i+=1
+    return base*sign
---
+++ Leetcode/Reversed Integer.py
@@ -0,0 +1,16 @@
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        rev=0
+        k=1
+        if x<0:
+            k=-1
+            x=-x
+        while(x!=0):
+            rev=rev*10+x%10
+            x=x/10
+            if(rev> 2147483647):
+                return 0
+        return rev*k
