652979d67670a4632c88592a4f755ee08058e55f
Leiyang <Leiyang@mokak.com>
2017-03-26 23:03:57 +0800

    WaterContainer

---
+++ Leetcode/WaterContainer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        n=len(height)
+        left=0
+        right=n-1
+        h=min(height[0],height[n-1])
+        ans=0
+        while(left<right):
+            h=min(height[left],height[right])
+            ans=max(ans,h*(right-left))
+            while(height[left]<=h and left<right):left+=1
+            while(height[right]<=h and left<right):right-=1
+        return ans
+            
+            
