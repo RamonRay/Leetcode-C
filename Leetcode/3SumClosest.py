4dcda2068f3781fb51355bc7fc2c6ccb4b66a3e4
Leiyang <Leiyang@mokak.com>
2017-03-27 12:34:47 +0800

    3SumClosest

---
+++ Leetcode/3SumClosest.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        ans=nums[0]+nums[1]+nums[len(nums)-1]
+        i=0
+        while i<len(nums)-2:
+            left=i+1
+            right=len(nums)-1
+            while(left<right):
+                out=nums[i]+nums[left]+nums[right]
+                if(out<target):
+                    left+=1
+                elif(out>target):
+                    right-=1
+                else:
+                    return target
+                if(abs(out-target)<abs(ans-target)):
+                    ans=out
+            i+=1
+        return ans
