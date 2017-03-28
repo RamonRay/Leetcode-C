2e42f03ddb141bc9eded5240c0d740828eeef421
Leiyang <Leiyang@mokak.com>
2017-03-29 01:07:13 +0800

    RemoveNthNode

---
+++ Leetcode/RemoveNthNode.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        x=[]
+        pointer=head
+        l=0
+        while(pointer!=None):
+            x.append(pointer)
+            pointer=pointer.next
+            l+=1
+        if n==l:
+            return head.next
+        if n==1:
+            x[l-2].next=None
+        else:
+            x[l-n-1].next=x[l-n+1]
+        return head
