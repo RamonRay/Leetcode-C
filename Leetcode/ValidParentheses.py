4e296f42cd1f51f7a7f20e15c716ee67f2aaab3b
Leiyang <Leiyang@mokak.com>
2017-03-29 01:35:57 +0800

    Valid Parentheses

---
+++ Leetcode/ValidParentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        x=[]
+        for i in s:
+            if i =='(':
+                x.append(')')
+            elif i=='[':
+                x.append(']')
+            elif i=='{':
+                x.append('}')
+            else:
+                if x==[] or i!=x.pop():
+                    return False
+        return x==[]
+                
