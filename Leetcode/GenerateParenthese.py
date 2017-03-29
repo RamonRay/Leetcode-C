683297c70f13800d74ea11fc6de014b60f52e62e
Leiyang <Leiyang@mokak.com>
2017-03-29 13:29:00 +0800

    no message

---
+++ Leetcode/GenerateParenthese.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        s=[]
+        def generate(tmp,left,right):
+            if left: generate(tmp+'(',left-1,right)
+            if left<right: generate(tmp+')',left,right-1)
+            if not right:
+                s.append(tmp)
+                return
+        generate('',n,n)
+        return s
